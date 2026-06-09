18b7371a893e0911d719a35488f1d51b920f6bbe
YOGESH <yogesh@yahoo.com>
2026-06-09 14:38:06 +0530

    terraform projects

--- ec2-project/ec2.tf
+++ ec2-project/ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "dev_ec2" {
+module "ec2" {
   source = "./modules/ec2"
 
   ami_id            = "ami-091138d0f0d41ff90"
@@ -13,13 +13,3 @@ module "dev_ec2" {
   instance_name     = "ec2-terraform"
 }
 
-module "prod_ec2" {
-  source = "./modules/ec2"
-
-  ami_id            = "ami-091138d0f0d41ff90"
-  instance_type     = "t3.micro"
-  subnet_id         = "subnet-0f634a055feef926e"
-  security_group_ids = ["sg-047c580e5c0638edd"]
-  key_name          = "YOGESH"
-  instance_name     = "ec3-terraform"
-}
